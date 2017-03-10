7ced3e36b17bc6a2c997846ba79c5192ee17acd9
ted-van-es98 <ted.van.es98@gmail.com>
2017-03-10 15:14:04 +0100

    hallo ted

---
+++ app/src/main/java/com/example/svanh/testproject/Aanmelden.java
@@ -0,0 +1,20 @@
+package com.example.svanh.testproject;
+
+/**
+ * Created by tedvanes on 10-03-17.
+ */
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class Aanmelden extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ted);
+
+    }
+}
