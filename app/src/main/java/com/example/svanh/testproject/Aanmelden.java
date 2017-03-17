4772464b761324186b3153b6604624e17ca5d392
ted-van-es98 <ted.van.es98@gmail.com>
2017-03-17 10:56:02 +0100

    hallo ted

--- app/src/main/java/com/example/svanh/testproject/Aanmelden.java
+++ app/src/main/java/com/example/svanh/testproject/Aanmelden.java
@@ -9,12 +9,28 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class Aanmelden extends AppCompatActivity{
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ted);
+
+        Button  Aanmelden = (Button)findViewById(R.id.Aanmelden);
+        Aanmelden.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "Bezig met Aanmelden", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Bezig met Aanmelden", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    public void Check(char User, char Pass) {
 
     }
 }
