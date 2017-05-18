ea4b6f7d224f9fb83aa2206faf5931951eca03c8
DavidHoeksema <112412@student.drenthecollege.nl>
2017-05-18 10:34:10 +0200

    david 3-4-17 13:11

---
+++ app/src/main/java/com/example/svanh/testproject/profiel/EditProfile.java
@@ -0,0 +1,57 @@
+package com.example.svanh.testproject.profiel;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.example.svanh.testproject.MainActivity;
+import com.example.svanh.testproject.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profiel);
+    }
+    public void onlick(View view) {
+        /*EditText wachtwoord = (EditText) findViewById(R.id.editText5);
+        wachtwoord.setOnEditorActionListener(new TextView.OnEditorActionListener(){
+            public boolean onEditorAction(TextView textview, int i, KeyEvent keyEvent){
+                boolean handled = false;
+                if(i == EditorInfo.IME_ACTION_NEXT){
+                    TextView textView =
+                    String inputText = textView.getText().tosString();
+                    Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                }
+            }*/
+
+        Context context = getApplicationContext();
+        final EditText input = (EditText)findViewById(R.id.editText6);
+        final EditText input2 = (EditText)findViewById(R.id.editText3);
+        final EditText input3 = (EditText)findViewById(R.id.editText8);
+        final EditText input4 = (EditText)findViewById(R.id.editText7);
+
+        String text = input.getText().toString() + ", " +  input2.getText().toString() + ", " + input3.getText().toString() + ", " +  input4.getText().toString();
+        int duration = Toast.LENGTH_SHORT;
+
+        final Toast toast = Toast.makeText(context, text, duration);
+        final Button button = (Button)findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+
+                 toast.show();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/svanh/testproject/profiel/Validator.java
@@ -0,0 +1,40 @@
+package com.example.svanh.testproject.profiel;
+
+/**
+ * Created by David on 18-5-2017.
+ */
+
+public class Validator {
+    String email;
+    int valid;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getValid() {
+        return valid;
+    }
+
+    public void setValid(int valid) {
+        this.valid = valid;
+    }
+
+
+    public Validator (String arg1) {
+        this.email = arg1;
+    }
+
+    public int isEmail(String arg1) {
+        // if email valid is dan dit:
+        //this.setValid(1);
+        // else
+        //this.setValid(0);
+        return this.getValid();
+    }
+}
+
