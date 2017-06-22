83652cb221b92b0b6e4922819adf78309b6de188
ted-van-es98 <ted.van.es98@gmail.com>
2017-06-22 11:00:42 +0200

    Login onclick

--- app/src/main/java/com/example/svanh/testproject/Login_Logout/Login.java
+++ app/src/main/java/com/example/svanh/testproject/Login_Logout/Login.java
@@ -2,14 +2,30 @@ package com.example.svanh.testproject.Login_Logout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
 
 import com.example.svanh.testproject.R;
 
 public class Login extends AppCompatActivity {
+    Button   mButton;
+    EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        mButton = (Button)findViewById(R.id.Aanmelden);
+        mEdit   = (EditText)findViewById(R.id.Username);
+
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+                    }
+                });
     }
 }
