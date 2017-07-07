dcdff461f020d525c1fb82e9b2368f79fe49aa95
svh1998 <s.vanhoogdalem@hotmail.com>
2017-07-07 11:22:02 +0200

    android manifest changes

--- app/src/main/java/com/example/svanh/testproject/Reserveringen/Nieuwe_Reserveringen.java
+++ app/src/main/java/com/example/svanh/testproject/Reserveringen/Nieuwe_Reserveringen.java
@@ -2,6 +2,8 @@ package com.example.svanh.testproject.Reserveringen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.svanh.testproject.R;
 
@@ -11,5 +13,9 @@ public class Nieuwe_Reserveringen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nieuwe__reserveringen);
+        Spinner dropdown = (Spinner)findViewById(R.id.spinner);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
     }
 }
