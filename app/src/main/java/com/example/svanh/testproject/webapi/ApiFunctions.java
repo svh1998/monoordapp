86a6ce32c85ad2d0dcf80164d1c77ea28ce5aa22
sandhoofd <sandhoofdnl@gmail.com>
2017-11-10 10:21:23 +0100

    .........

--- app/src/main/java/com/example/svanh/testproject/webapi/ApiFunctions.java
+++ app/src/main/java/com/example/svanh/testproject/webapi/ApiFunctions.java
@@ -1,84 +1,61 @@
 package com.example.svanh.testproject.webapi;
 
-        import android.app.ActivityManager;
-        import android.content.BroadcastReceiver;
-        import android.content.Context;
-        import android.content.Intent;
-        import android.content.IntentFilter;
-        import android.os.Build;
-        import android.os.SystemClock;
-        import android.provider.ContactsContract;
-        import android.support.annotation.RequiresApi;
-        import android.support.v4.content.LocalBroadcastManager;
-        import android.util.Log;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.SystemClock;
+import android.provider.ContactsContract;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        import java.lang.reflect.Array;
-        import java.net.MalformedURLException;
-        import java.util.ArrayList;
-        import java.util.List;
+import java.lang.reflect.Array;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by svanh on 6/8/2017.
- */
 
 public class ApiFunctions {
-    static String weburl = "http://api.hostdalem.nl/";
+
+
+    static String weburl = "https://sandhoofd.nl/api/";
+
+
+    // user functions
+
 
     public static void login(Context context, String email, String wachtwoord){
         Intent intent = new Intent(context, Driver.class);
         intent.putExtra("url", weburl + "validate.php?email="+email+"&password="+wachtwoord);
         context.startService(intent);
+    }
 
+    public static void getUserData(Context context, String email) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl + "getuserdata.php?email="+email);
+        context.startService(intent);
     }
 
     public static void Register(Context context,
                                 String email, String password, String firstname, String lastname, String streetname, int housenumber,
                                 String housenrext, String zipcode, String City, int netnumber, int phonenumber) {
-        Log.d("Register FUnction", "Incoming");
         Intent intent = new Intent(context, Driver.class);
         intent.putExtra("url", weburl+"register.php?email="+email+"&password="+password+"&firstname="+firstname+"&lastname="+lastname+
                 "&streetname="+streetname+"&housenumber="+housenumber+"&nrext="+housenrext+"&zipcode="+zipcode+"&city="+City+
                 "&phone="+netnumber+"-"+phonenumber);
         context.startService(intent);
-
     }
+
     public static void ChangePassword(Context context, int userid, String currentpw, String newpw, String confirmpw) {
         Intent intent = new Intent(context, Driver.class);
         intent.putExtra("url", weburl+"changepassword.php?user="+userid+"&currentpw="+currentpw+"&newpw="+newpw+"&confirmpw="+confirmpw);
-        context.startService(intent);
-    }
-
-    public static void getBands(Context context, int userid) {
-        Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl+"getbands.php?user="+userid);
-        context.startService(intent);
-    }
-
-    public static void createReservation(Context context){
-        Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl + "addreservation.php");
-        context.startService(intent);
-    }
-
-    public static void askRecovery(Context context) {
-        Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl + "recover.php");
-        context.startService(intent);
-    }
-
-    public static void checkToken(Context context, String token) {
-        Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl + "checktoken.php?token="+token);
-        context.startService(intent);
-    }
-
-    public static void recoverPassword(Context context, String password, String passwordconfirm) {
-        Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl + "resetpassword.php?password="+password+"&confirmpw="+passwordconfirm);
         context.startService(intent);
     }
 
@@ -88,48 +65,78 @@ public class ApiFunctions {
         intent.putExtra("url", weburl + "checkemail.php?email="+email);
         context.startService(intent);
     }
-    public static void AddReservation(Context context, int bandid, String paymentmethod ) {
+
+    public static void askRecovery(Context context, String email) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl + "recover.php?email="+email);
+        context.startService(intent);
+    }
+
+    public static void checkToken(Context context, String email, String token) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl + "checktoken.php?token="+token + "&email="+email);
+        context.startService(intent);
+    }
+
+    public static void resetPassword(Context context, String password, String passwordconfirm) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl + "resetpassword.php?password="+password+"&confirmpw="+passwordconfirm);
+        context.startService(intent);
+    }
+
+
+    // band functions
+
+
+    public static void getBands(Context context, int userid) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl+"getbands.php?user="+userid);
+        context.startService(intent);
+    }
+
+    public static void Addband(Context c, String bandname, String notes) {
+        Intent intent = new Intent(c, Driver.class);
+        intent.putExtra("url", weburl+"addband.php?bandname="+bandname+"&notes="+notes);
+    }
+
+
+    // reservation functions
+
+
+
+    public static void getPaymentMethods(Context context) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl+"getallpaymentmethods.php");
+        context.startService(intent);
+    }
+    public static void getReservations(Context context, int band) {
+        Intent intent = new Intent(context, Driver.class);
+        intent.putExtra("url", weburl+"getreservation.php?band_id=4");
+        context.startService(intent);
+    }
+
+    public static void AddReservation(Context context, int bandid, String paymentmethod, int roomid, String datum, String begintijd, String eindtijd) {
         Intent intent = new Intent(context, Driver.class);
         intent.putExtra("url", weburl + "addreservation.php?bandid="+bandid + "&paymentmethod="+paymentmethod);
         context.startService(intent);
     }
 
     public static void getRooms(Context context) {
-        
-    }
-
-    public static void getReservations(Context context, int band) {
         Intent intent = new Intent(context, Driver.class);
-        intent.putExtra("url", weburl+"getReservation.php?band_id=4");
+        intent.putExtra("url", weburl+"getrooms.php");
         context.startService(intent);
     }
 
-
-    public ArrayList<String> setData(String broadcastResult) {
-        ArrayList<String> arrayList = new ArrayList<String>();
-        JSONObject jObject = null;
-        JSONArray jsonArray= null;
-        try {
-            jObject = new JSONObject(broadcastResult);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            jsonArray = new JSONArray(jObject);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-//        List<String> list = new ArrayList<String>();
-//        for(int i=0; i <jsonArray.length(); i++) {
-//            product.setId(??);
-//            product.setName(??);
-//            list.add(??);
-//        }
-        Log.d("Array: ", ""+arrayList);
-        return arrayList;
+    public static void CheckAvailability(Context c, int roomid) {
+        Intent intent = new Intent(c, Driver.class);
+        intent.putExtra("url", weburl+"checkroomavailability.php?id="+roomid);
+        c.startService(intent);
     }
+
+
+
+// parse array
+
 
     public static String getArrayData(String broadcastResult, String veld){
         JSONObject jObject = null;
